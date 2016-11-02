0f3140acc7d01c55840a9ea9629743019ec82a0a
Borja Lorente <blorente@ucm.es>
2016-11-02 09:52:09 +0100

    Add gradient to cost

--- lab2/cost.m
+++ lab2/cost.m
@@ -3,5 +3,8 @@ function [J, grad] = cost(theta, X, y)
   hypotheses = sigmoid(dot(extended_theta, X, 2));
   positive_hypotheses = y.*log(hypotheses);
   negative_hypotheses = (1 - y).*log(1 - hypotheses);
-  J = 1/rows(X) * sum(-positive_hypotheses - negative_hypotheses)
+  J = 1/rows(X) * sum(-positive_hypotheses - negative_hypotheses);
+  grad1 = 1/rows(X) * sum((hypotheses - y) .* X(:, 1));
+  grad2 = 1/rows(X) * sum((hypotheses - y) .* X(:, 2));
+  grad = [grad1, grad2];
 endfunction
