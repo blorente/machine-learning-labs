7140308835976e1edeef87fbbb1f53b434e502a6
Usuario Local <usuario_local@localhost.localdomain>
2016-11-17 15:38:01 +0100

    Add count correct

--- lab3/part_a.m
+++ lab3/part_a.m
@@ -1,7 +1,7 @@
 load('files/ex3data1.mat');
 
 m = size(X, 1);
-lambda = 0.001;
+lambda = 0.000;
 theta = zeros(columns(X) + 1, 1);
 
 % Randomly select 100 examples
@@ -13,3 +13,4 @@ sel = X(rand_indices(1:100), :);
 X_ext = [ones(rows(X), 1) X];
 %[J, grad] = lrCostFunction(theta, X_ext, y, lambda);
 [all_theta] = oneVsAll(X_ext, y, 10, lambda);
+correct = countCorrect(X_ext, y, all_theta)
