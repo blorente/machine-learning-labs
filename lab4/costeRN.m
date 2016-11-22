aead819dd3d9dc8eb688b8d9cb425b4ba24cbd1c
Borja Lorente <blorente@ucm.es>
2016-11-22 11:40:20 +0100

    Add regularization to cost NN

--- lab4/costeRN.m
+++ lab4/costeRN.m
@@ -1,12 +1,20 @@
 function [J, grad] = costeRN(params_rn, num_attribs, nodos_ocultos, num_etiquetas, X, y, lambda)
+  % Capturar las matrices de pesos originales
   Theta1 = reshape (params_rn(1:nodos_ocultos * (num_attribs + 1)), nodos_ocultos, (num_attribs + 1));
   Theta2 = reshape (params_rn((1 + (nodos_ocultos * (num_attribs + 1))):end), num_etiquetas, (nodos_ocultos + 1));
-  hypothesis_matrix = hypothesis(X, Theta1, Theta2);
+  % Calcular las hipótesis mediante la propagación
+  hypotheses = hypothesis(X, Theta1, Theta2);
+  % Calcular el coste por cada etiqueta
   J = 0;
   for i = 1:num_etiquetas
-    partial_positive = y(:, i) .* log(hypothesis_matrix(:, i));
-    partial_negative = (1 - y(:, i)) .* log(1 - hypothesis_matrix(:, i));
-    J = J + sum(- partial_positive - partial_negative)
+    J = J + sum(-y(:, i).*log(hypotheses(:, i)) - ((1 - y(:, i)).*log(1 - hypotheses(:, i))));
   endfor
-  grad = Theta2;
+  % Ajustar al número de ejemplos
+  m = rows(X);
+  J = (1 / m) * J;
+  % Aplicar parámetro de regularización
+  Theta1_trim = Theta1(:, 2:columns(Theta1));
+  Theta2_trim = Theta2(:, 2:columns(Theta2));
+  J = J + ((lambda/(2*m))*(sum(sum(Theta1_trim .* Theta1_trim)) + sum(sum(Theta2_trim .* Theta2_trim))));
+  grad = Theta2(:, 2:end);
 endfunction
--- lab4/part_a.m
+++ lab4/part_a.m
@@ -4,11 +4,10 @@ load('files/ex4data1.mat');
 load('files/ex4weights.mat');
 
 % Parámetros iniciales
-lambda = 0.01;
+lambda = 1;
 num_etiquetas = 10;
 
 % Transformaciones de entrada para usar en funciones
-%X_ext = [ones(rows(X), 1) X];
 y_unrolled = zeros(rows(y), num_etiquetas);
 % Por cada valor de y, marcarlo en y_unrolled
 for i = 1:num_etiquetas
@@ -16,5 +15,7 @@ for i = 1:num_etiquetas
   y_unrolled(:, i) = marks;
 endfor
 
+% Crear vector inicial de thetas:
 Thetas = [Theta1(:); Theta2(:)];
-[J, grad] = costeRN(Thetas, columns(Theta1) - 1, rows(Theta1), num_etiquetas, X, y_unrolled, lambda);
+%Thetas = [zeros(size(Theta1))(:); zeros(size(Theta2))(:)];
+[J, grad] = costeRN(Thetas, columns(Theta1) - 1, rows(Theta1), num_etiquetas, X, y_unrolled, lambda)
