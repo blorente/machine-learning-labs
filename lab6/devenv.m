897d99c38aa6397b1bf0524adf3afd77ba9ae66f
Borja Lorente <blorente@ucm.es>
2016-12-05 21:29:19 +0100

    Add part_b files

--- lab6/devenv.m
+++ lab6/devenv.m
@@ -1,40 +1,19 @@
-% Section 1.3 -> C and sigma choice
+% Section 2.1 -> Process emails
 
-% Load training data
-load('files/ex6data3.mat');
-m = rows(X);
+% Load spam data
+spam_filelist = readdir('files/spam');
 
-% C and sigma value pool
-seeds = [0.01; 0.03];
-iterations = 3;
-pool = zeros(size(seeds), iterations);
-for i = 0:iterations
-  pool(:, i + 1) = seeds .* (10 ^ i);
+printf('Processing spam');
+for email = 1:numel(spam_filelist)
+  printf('.');
+  % Skip special files . and ..
+  if (regexp (spam_filelist{email}, "^\\.\\.?$"))
+    continue;
+  endif
+  % Load file
+  filename = sprintf('files/spam/%s', spam_filelist{email});
+  raw = readFile(filename);
+  processed = processEmail(raw);
+
 endfor
-% Vectorize pool
-poolsize = numel(pool);
-pool = reshape(pool, 1, poolsize);
-
-% Generate models in order
-fig = 1;
-for C = pool
-  for sigma = pool
-    % Tain machine and generate prediction
-    model_gaussian = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-    prediction = svmPredict(model_gaussian, X);
-
-    % Calculate correctness and output result
-    correct = sum(y == prediction);
-    fraction = correct / m;
-    printf('C: %f\nsigma: %f\ncorrect: %f\nperc. of total: %.2f\n', C, sigma, correct, (fraction * 100));
-
-    % Uncomment to see the graphs
-    %figure(fig); fig++;
-    %visualizeBoundary(X, y, model_gaussian);
-  endfor
-endfor
-
-printf('Models generated!\n');
-
-waitPress();
-close all;
+printf('Done!\n');
