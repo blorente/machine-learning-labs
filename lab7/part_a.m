16a5869990c5020bad90ee31ebedb9ffc0da2a97
Borja Lorente <blorente@ucm.es>
2016-12-19 20:54:56 +0100

    mend

--- lab7/part_a.m
+++ lab7/part_a.m
@@ -4,7 +4,9 @@ K = 3;
 randidx = randperm(size(X,  1));
 centroids = X(randidx(1:K) , :);
 
+% Test
 closestCentroids = findClosestCentroids(X, centroids);
 newCentroids = computeCentroids(X, closestCentroids, K);
 
+% Test
 [centroids, idx] = runkMeans(X, [3 3; 6 2; 8 5], 10, true);
